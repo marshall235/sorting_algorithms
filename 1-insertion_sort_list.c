19f5b487171f95036ad833f405ef782408325106
marshall235 <fredie1738@gmail.com>
2024-04-28 09:12:40 +0300

    Insertion Algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,11 +13,10 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-
 	if ((*n1)->prev != NULL)
 		(*n1)->prev->next = n2;
 	else
-		*h2 = n2;
+		*h = n2;
 	(*n1)->prev = n2;
 	*n1 = n2->prev;
 }
@@ -29,13 +28,13 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  *
  * Description: Prints the list after each swap.
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *temp;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
+
 	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
